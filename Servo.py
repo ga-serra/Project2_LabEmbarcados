81cf2c8874e5d545c2f52441eb2d1b72c497f420
ga-serra <gabrielvserra25@gmail.com>
2025-05-12 21:00:19 -0300

    Smooth movement implemented

--- Servo.py
+++ Servo.py
@@ -1,22 +1,35 @@
 from machine import Pin, PWM
+import time
 
 class Servo:
     def __init__(self, pin_num:int, freq:int = 50):
         self.pwm = PWM(Pin(pin_num))
         self.pwm.freq(freq)
+        self.current_angle = 0
 
-    def set_angle(self, angle):
+    def set_angle(self, angle,delay=0.01, step_size=1):
         # Angle has to be between 0º and 180º
         if angle < 0:
             angle = 0
         elif angle > 180:
             angle = 180
 
+        step = step_size if angle > self.current_angle else -step_size
+
+        for ang in range(self.current_angle, angle, step):
+            self.write_angle(ang)
+            time.sleep(delay)
+        
+        self.write_angle(angle)
+        self.current_angle = angle
+
+    def write_angle(self, angle):
         # angle for time (pulse)
         min_time = 500                  # minimum pulse time
         max_time = 2500                 # maximum pulse time
         time_us = min_time + (angle/180) * (max_time - min_time)
 
+        
         # Set duty cycle
         duty_cycle = int(time_us * 65535 / 20000)
-        self.pwm.duty_u16(duty_cycle)
+        self.pwm.duty_u16(duty_cycle)        
